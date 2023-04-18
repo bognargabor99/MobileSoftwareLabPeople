4184ab7f98dbe4ff0c7433a2a8cb20ff0ad2a4a0
Gabor Bela Bognar <gaborbognar99@gmail.com>
2023-04-18 23:30:46 +0200

    Add UI skeleton

--- app/src/main/java/hu/bme/aut/mszl/people/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/mszl/people/MainActivity.kt
@@ -3,44 +3,33 @@ package hu.bme.aut.mszl.people
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import hu.bme.aut.mszl.people.ui.theme.PeopleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PeopleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
+fun PeopleApp(
+    modifier: Modifier = Modifier
+) {
     PeopleTheme {
-        Greeting("Android")
+        val navController = rememberNavController()
+        NavHost(
+            modifier = modifier,
+            navController = navController,
+            startDestination = ""
+        ) {
+
+        }
     }
 }
---
+++ app/src/main/java/hu/bme/aut/mszl/people/ui/screen/list/PeopleListRepository.kt
@@ -0,0 +1,8 @@
+package hu.bme.aut.mszl.people.ui.screen.list
+
+import javax.inject.Inject
+
+class PeopleListRepository @Inject constructor(
+
+) {
+}
