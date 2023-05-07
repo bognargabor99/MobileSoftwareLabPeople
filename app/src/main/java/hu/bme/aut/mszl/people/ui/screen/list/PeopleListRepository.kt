02dc35a9473f6bba80a89e76f7550bf28d63e590
Gabor Bela Bognar <gaborbognar99@gmail.com>
2023-05-07 02:52:07 +0200

    Database and network wiring into repositories

--- app/src/main/java/hu/bme/aut/mszl/people/ui/screen/list/PeopleListRepository.kt
+++ app/src/main/java/hu/bme/aut/mszl/people/ui/screen/list/PeopleListRepository.kt
@@ -1,8 +1,27 @@
 package hu.bme.aut.mszl.people.ui.screen.list
 
+import hu.bme.aut.mszl.people.network.RandomUserApi
+import hu.bme.aut.mszl.people.persistence.PeopleDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class PeopleListRepository @Inject constructor(
-
+    private val randomUserApi: RandomUserApi,
+    private val peopleDao: PeopleDao
 ) {
+    suspend fun getPeople() =
+        withContext(Dispatchers.IO) {
+            randomUserApi.getPeople(50).results
+        }
+
+    suspend fun getCategory(name: String) =
+        withContext(Dispatchers.IO) {
+            peopleDao.getCategory(name)
+        }
+
+    suspend fun getCategoryNames() =
+        withContext(Dispatchers.IO) {
+            peopleDao.getCategories().map { it.name }
+        }
 }
